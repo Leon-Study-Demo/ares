9ca5468172d687e0366ad9b1253ad2fab0be4650
leonseki <leonstokyo@gmail.com>
2024-03-02 14:06:26 +0900

    init

---
+++ mybatis-generator/src/main/java/jp/tokyo/leon/cloud/mapper/PayMapper.java
@@ -0,0 +1,11 @@
+package jp.tokyo.leon.cloud.mapper;
+
+import jp.tokyo.leon.cloud.entity.Pay;
+import tk.mybatis.mapper.common.Mapper;
+
+
+/**
+ * @author leon
+ */
+public interface PayMapper extends Mapper<Pay> {
+}
